23ffc1d730505195a7b3683258ec7020a5106c62
sungj <https://github.com/sungj1104>
2024-05-30 11:00:48 +0900

    챗 뷰 = 로그인 되어있지 않을때의 활동 추가 챗 인덱스 html = 로그인 되어 있지 않을때의 자바스크립트 추가 admin페이지 user 정보가 보이게 변경 users.models 모델 받지 않는정보를 삭제 (마이그레이션 삭제)

--- FINAL/Final/chat/views.py
+++ FINAL/Final/chat/views.py
@@ -15,29 +15,34 @@ def index(request):
         "room_list": room_qs,
     })
 
-@login_required
 def room_chat(request, room_pk):
-    room = get_object_or_404(Room, pk=room_pk)
-    return render(request, "chat/room_chat.html", {
-        "room": room,
-    })
-
-@login_required
-def room_new(request):
-    if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            created_room: Room = form.save(commit=False)
-            created_room.owner = request.user
-            created_room.save()
-            # room pk 기반으로 채팅방 URL을 만듦
-            return redirect("chat:room_chat", created_room.pk)
+    if request.user.is_authenticated:
+        room = get_object_or_404(Room, pk=room_pk)
+        return render(request, "chat/room_chat.html", {
+            "room": room,
+        })
     else:
-        form = RoomForm()
+        return redirect("users:login") 
 
-    return render(request, "chat/room_form.html", {
-        "form": form,
-    })
+def room_new(request):
+    if request.user.is_authenticated: # 로그인 되어 있다면
+        if request.method == "POST":
+            form = RoomForm(request.POST)
+            if form.is_valid():
+                created_room: Room = form.save(commit=False)
+                created_room.owner = request.user
+                created_room.save()
+                # room pk 기반으로 채팅방 URL을 만듦
+                return redirect("chat:room_chat", created_room.pk)
+        else:
+            form = RoomForm()
+
+        return render(request, "chat/room_form.html", {
+            "form": form,
+        })
+    else:
+        return redirect("users:login")
+
 
 @login_required
 def room_delete(request, room_pk):
@@ -46,13 +51,9 @@ def room_delete(request, room_pk):
     # 권한을 체크하는 방법은 다양하며, 다양한 철학의 라이브러리가 있음
     # 소유자에게만 삭제버튼을 노출하는 것만으로 충분하지 않음. URL을 예상하여 요청이 들어올 수 있음
     # 프론트 단에서의 채킹과 별개로 백엔드 단에서의 권한 체크는 필수
-    if room.owner != request.user:
-        messages.error(request, "채팅방 소유자가 아닙니다.")
-        return redirect("chat:index")
-
+    
     if request.method == "POST":
         room.delete() # HARD DELETE : 데이터베이스에서 삭제
-        messages.success(request, "채팅방을 삭제했습니다.")
         return redirect("chat:index")
 
     return render(request, "chat/room_confirm_delete.html", {
--- FINAL/Final/users/admin.py
+++ FINAL/Final/users/admin.py
@@ -3,3 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None, {"fields" : ("username", "password")}),
+        ("개인정보", {"fields": ("email",)}),
+        ("권한", {"fields": ("is_active", "is_staff", "is_superuser")}),
+    ]
