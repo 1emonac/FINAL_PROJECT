a2a4f53b315dca9df9cc265de97ffb6de530998e
sungj <https://github.com/sungj1104>
2024-06-10 09:12:07 +0900

    상담페이지 제작

--- FINAL/Final/survey/views.py
+++ FINAL/Final/survey/views.py
@@ -3,6 +3,9 @@ import os
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import SurveyRoom
+import joblib
+import pandas as pd
+import numpy as np
 
 # OpenAI API 키 설정
 openai.api_key = os.environ.get("OPENAI_API_KEY")
@@ -40,7 +43,18 @@ def example(request):
     return render(request, 'survey/survey3.html')
 
 def example2(request):
-    return render(request, 'survey/survey4.html')
+   if request.method == 'POST':
+        # POST 요청으로 전송된 데이터 가져오기
+        lr = joblib.load(os.path.join('static', "model", 'logistic_regression_model.pkl'))
+        pred_df = list()
+        for key, value in request.POST.items():
+            if "token" not in key:
+                pred_df.append(value)
+        pred_df = pd.DataFrame(pred_df).T
+        print(lr.predict_proba(pred_df)[0][1])
+        pred_df = np.round(lr.predict_proba(pred_df)[0][1] * 100).astype(int)
+        context = {"predict" : pred_df}
+        return render(request, 'survey/survey4.html', context)
 
 def survey5(request):
     return render(request, 'survey/survey5.html')
